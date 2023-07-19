e9426aec4dcbe9f807b74ba13d5b0c21c8496e6f
Ravuri Teja Nagaswi <111522542+Tejaswi0418@users.noreply.github.com>
2023-07-19 10:39:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function bud()
+{
+      document.getElementById("flower").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeVycVS84ebFNA5-60yVnD0dqlAISnK1t8xA&usqp=CAU";
+}
+function fullflower()
+    {
+        document.getElementById("flower").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlrpAOX4R2eWTCfpZXleoJzrtnBIg2Iyt0Nw&usqp=CAU";
+    }
+function petals()
+{
+    document.getElementById("flower").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyn8cBY33aoHvfqCk6-5y2wmgHEivSL48ibA&usqp=CAU";
+}
+
